613f816e0181d0ca4a2637c9683de905525b2707
inakicarcereny <inakicarce@hotmail.com.ar>
2024-05-17 19:27:12 -0300

    separate icons

---
+++ src/icons/Arrow1.jsx
@@ -0,0 +1,13 @@
+export function Arrow1 () {
+  
+  return (
+    <div className="bg-blue-500 rounded-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" stroke="#000000" fill="none">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M5 12l14 0" />
+        <path d="M13 18l6 -6" />
+        <path d="M13 6l6 6" />
+      </svg>
+    </div>
+  )
+}
---
+++ src/icons/UserIcon.jsx
@@ -0,0 +1,10 @@
+export function UserIcon () {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" stroke="#000000" fill="none">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+</svg>
+  )
+}
