f91149ce64a7919897a96f8d9f7ad7eaf8fc6c4a
inakicarcereny <inakicarce@hotmail.com.ar>
2024-05-20 17:19:43 -0300

    change the render movie from mock to data fetch

--- src/components/RenderMovies.jsx
+++ src/components/RenderMovies.jsx
@@ -1,12 +1,9 @@
-import results from '../mocks/results.json'
-
-export function RenderMovies() {
-  const movies = results.Search
+export function RenderMovies({movies}) {
 
   return (
       <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-4'>
         {
-          movies.slice(0, 6).map((movie) => (
+          movies.map((movie) => (
             <li 
             className='bg-gray-200 rounded-xl py-2 flex flex-col justify-center items-center hover:scale-105 duration-200'
             key={movie.imdbID}>
